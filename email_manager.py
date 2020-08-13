12d68f96b132326b3764098083d937b05704673d
jhendle2 <jhendl2@binghamton.edu>
2020-08-12 21:38:11 -0400

    Fixed stuff

--- email_manager.py
+++ email_manager.py
@@ -1,9 +1,11 @@
 import smtplib, ssl
+import login
 
 port = 465  # For SSL
-password = "plantasia2cr2&a"
-sender_email = "cr2emailer@gmail.com"
-receiver_email = "td@whrwfm.org"
+# password
+# sender_email = "cr2emailer@gmail.com"
+# td_email = "td@whrwfm.org"
+# prod_email = "production@whrwfm.org"
 
 # Create a secure SSL context
 context = ssl.create_default_context()
@@ -13,5 +15,15 @@ def send_login_email(engineer, time):
     email = "Subject: CR2 Login - %s\n\n%s logged into CR2 at %s" % (engineer, engineer, time)
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, email)
-    print("! Sent login email to %s" % receiver_email)
+        server.sendmail(sender_email, td_email, email)
+    print("! Sent login email to %s" % td_email)
+
+
+def send_error_email(error, time):
+    email = "Subject: CR2 Problem - %s\n\n%s" % (time, error)
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, td_email, email)
+        # server.sendmail(sender_email, prod_email, email)
+    print("! Problem reported to %s" % td_email)
+    # print("! Problem reported to %s" % prod_email)
